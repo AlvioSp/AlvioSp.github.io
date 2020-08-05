7194e4e9a35dc27fcb903fb708ff4e9f4426769c
AlvioSp <68581197+AlvioSp@users.noreply.github.com>
2020-08-04 23:58:58 -0500

    w3d2
    w3d2

---
+++ w3d2/BubblePopper.js
@@ -0,0 +1,46 @@
+
+$(function () {
+
+    const $numbercircles = $("#numbercircles");
+
+    $('#start').click(function () {       
+            for (let i = 0; i < parseInt($numbercircles.val()); i++) {
+                createCircle();
+            }
+    });
+
+});
+
+
+function createCircle() {
+    const $width = $("#width");
+    const $growthamount = $("#growthamount");
+    const $growrate = $("#growrate");
+
+    const $div = $('<div>', { class: 'circle' });
+
+    $('#circles').append($div);
+    $div.width($width.val());
+    $div.height($width.val());
+
+    let backc = "rgb(" + ramdom() + ", " + ramdom() + ", " + ramdom() + ")";
+    $div.css("background-color", backc);
+    $div.css("right", ramdom(10, 800) + "px");
+
+    const id = setInterval(function () {
+        let grow = $div.width() + parseInt($growthamount.val());
+        $div.width(grow);
+        $div.height(grow);
+    }, $growrate.val());
+
+    $div.click(function () {
+        $div.remove();
+        clearInterval(id);
+    });
+}
+
+function ramdom(min = 0, max = 255) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
